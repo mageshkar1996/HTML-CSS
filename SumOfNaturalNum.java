b53ae377961e5b910755b628fe88589129b15367
Mageshkar <96710996+mageshkar1996@users.noreply.github.com>
2022-01-13 23:11:11 +0530

    Add files via upload
    Sum of Natural Numbers

---
+++ SumOfNaturalNum.java
@@ -0,0 +1,13 @@
+public class SumOfNaturalNum
+{
+public static void main(String[] args)
+{       int sum=0;
+        for (int num=1;num<=10;num++)
+        {
+            sum=sum+num;
+        }
+        System.out.println("total value of natural number is:"+" "+sum);
+    
+}
+
+}
